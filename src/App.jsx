eab394aa951a467267ca6f42bdff1a3719a6d6a1
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-06-12 21:00:26 +0200

    Scroll to section

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import { Canvas } from "@react-three/fiber";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import { Experience } from "./components/Experience";
 import Interface from "./components/Interface";
+import ScrollManager from "./components/ScrollManager";
 
 function App() {
-  const [pages, setPages] = useState(0);
+  const [section, setSection] = useState(0);
 
   return (
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-      <color attach="background" args={["#a11e1e"]} />
-      <ScrollControls pages={4} damping={0.1}>
-        {/* <ScrollManager/> */}
-        <Experience />
-        <Scroll html>
-          <Interface />
-        </Scroll>
-      </ScrollControls>
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+        <color attach="background" args={["#a11e1e"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <ScrollManager section={section} onSectionChange={setSection} />
+          <Experience />
+          <Scroll html>
+            <Interface />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+    </>
   );
 }
 
