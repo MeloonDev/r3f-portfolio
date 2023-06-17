28c01629caca1ff1f3ede5160d145a5e479378f6
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-06-17 21:02:39 +0200

    Background animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,12 @@
-import { ContactShadows, Environment, useScroll } from "@react-three/drei";
-import { Leva, useControls } from "leva";
+import { ContactShadows, useScroll } from "@react-three/drei";
 import { motion } from "framer-motion-3d";
 import { useFrame, useThree } from "@react-three/fiber";
 import { animate, useMotionValue } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { framerMotionConfig } from "../config";
-import * as THREE from "three";
 import { Avatar } from "./Avatar";
+import Projects from "./Projects";
+import Background from "./Background";
 
 export const Experience = (props) => {
   const { menuOpened } = props;
@@ -18,26 +18,29 @@ export const Experience = (props) => {
   const [section, setSection] = useState(0);
 
   const [characterAnimation, setCharacterAnimation] = useState("Waving");
-  const animations = ["Waving", "Idle", "Idle", "Idle"];
+  const animations = ["Waving", "Idle", "Idle", "Calling"];
   useEffect(() => {
     setCharacterAnimation("Falling");
     setTimeout(() => {
       setCharacterAnimation(animations[section]);
-    }, 600);
+    }, duration * 1000);
   }, [section]);
-  // const { animation } = useControls({
-  //   animation: {
-  //     value: "Waving",
-  //     options: ["Waving", "Idle", "Falling"],
-  //   },
-  // });
+
+  const [duration, setDuration] = useState(2);
+  useEffect(() => {
+    setDuration(0.6);
+  }, []);
 
   const cameraPositionX = useMotionValue();
   const cameraLookAtX = useMotionValue();
 
   useEffect(() => {
-    animate(cameraPositionX, menuOpened ? -1 : 0, { ...framerMotionConfig });
-    animate(cameraLookAtX, menuOpened ? 1 : 0, { ...framerMotionConfig });
+    animate(cameraPositionX, menuOpened ? -1 : 0, {
+      ...framerMotionConfig,
+    });
+    animate(cameraLookAtX, menuOpened ? 1 : 0, {
+      ...framerMotionConfig,
+    });
   }, [menuOpened]);
 
   useFrame((state) => {
@@ -52,29 +55,26 @@ export const Experience = (props) => {
     if (curSection !== section) {
       setSection(curSection);
     }
-
-    // const position = new THREE.Vector3();
-    // characterContainer.current.getWorldPosition(position);
-
-    // const rotation = new THREE.Quaternion();
-    // characterContainer.current.getWorldQuaternion(rotation);
-    // const euler = new THREE.Euler().setFromQuaternion(rotation, "XYZ");
   });
 
   return (
     <>
-      <Leva hidden />
-      <ambientLight intensity={1} />
+      <Background />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 10, 0]} intensity={0.8} />
       <motion.group
         ref={characterContainer}
-        position={[0, -1, 0]}
+        position={[0, 8, 0]}
         transition={{
-          duration: 0.6,
+          duration: duration,
           delay: 0,
         }}
         animate={"" + section}
         variants={{
           0: {
+            y: -1,
+            x: 0,
+            z: 0,
             scaleX: 1.1,
             scaleY: 1.1,
             scaleZ: 1.1,
@@ -89,13 +89,13 @@ export const Experience = (props) => {
           },
           2: {
             y: section * -viewport.height - 1,
-            x: 0,
+            x: -2,
             z: 0,
-            scaleX: 1,
+            rotateY: Math.PI * 0.5,
           },
           3: {
             y: section * -viewport.height - 0.3,
-            x: 0,
+            x: 0.6,
             z: 3,
             rotateY: -Math.PI * 0.2,
           },
@@ -111,6 +111,7 @@ export const Experience = (props) => {
         /> */}
         <Avatar animation={characterAnimation} />
       </motion.group>
+      <Projects />
     </>
   );
 };
